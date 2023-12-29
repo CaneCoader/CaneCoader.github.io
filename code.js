23ecb77564e4e08b59117d983eb1225bc0cfcaec
Beans <142376407+CaneCoader@users.noreply.github.com>
2023-12-29 19:37:05 +1100

    anims

--- code.js
+++ code.js
@@ -5,7 +5,21 @@ var r = document.querySelector(':root');
 if (shiny){
     document.documentElement.className = "shiny";
     document.getElementById("shinymsg").style.display = "block"; 
-    scrollV = window.scroll(0,0);
-
 }
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('grow-anim');
+      }
+    });
+  });
+
+
+const boxes = document.querySelectorAll('.box-effect');
+boxes.forEach(box => {
+    observer.observe(box);
+});
+
+
 
